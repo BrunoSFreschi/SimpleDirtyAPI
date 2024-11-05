9bfe789fb7537ac42e755c377eef9f6e187ff5cc
BrunoFreschiAgger <bruno.freschi2@gmail.com>
2024-11-04 23:49:59 -0300

    update

---
+++ SimpleAPI/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleAPI.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Phone { get; set; }
+    public required string Adress { get; set; }
+    public DateTime CreateAt { get; set; } = DateTime.Now;
+}
