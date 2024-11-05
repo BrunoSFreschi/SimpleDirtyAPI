f41b3e0d4726deb375e6b5d3ceb082c757405c43
BrunoFreschiAgger <bruno.freschi2@gmail.com>
2024-11-04 23:49:51 -0300

    create appdb

---
+++ SimpleAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleAPI.Models;
+
+namespace SimpleAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+       optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+}
