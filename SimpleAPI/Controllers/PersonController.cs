ef4ae2e1dadf1025717c0f3fe72dde55002fd52d
BrunoFreschiAgger <brunofreschi@agger.com.br>
2024-11-07 10:09:22 -0300

    create getasync

---
+++ SimpleAPI/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleAPI.Data;
+
+namespace SimpleAPI.Controllers;
+
+[Route("/v1")]
+[ApiController]
+public class PersonController : ControllerBase
+{
+    [HttpGet]
+    [Route("/persons")]
+    public async Task<IActionResult> GetAsync(AppDbContext context)
+    {
+        var persons = await context.Persons.AsNoTracking().ToListAsync();
+
+        return Ok(persons);
+    }
+}
