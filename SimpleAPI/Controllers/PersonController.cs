b9f16535d56cf6c1a976c864f35466867f566dda
BrunoFreschiAgger <brunofreschi@agger.com.br>
2024-11-07 10:23:10 -0300

    create ById

--- SimpleAPI/Controllers/PersonController.cs
+++ SimpleAPI/Controllers/PersonController.cs
@@ -10,10 +10,19 @@ public class PersonController : ControllerBase
 {
     [HttpGet]
     [Route("/persons")]
-    public async Task<IActionResult> GetAsync(AppDbContext context)
+    public async Task<IActionResult> GetAsync([FromServices] AppDbContext context)
     {
         var persons = await context.Persons.AsNoTracking().ToListAsync();
 
         return Ok(persons);
+    }
+
+    [HttpGet]
+    [Route("/persons/{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromServices] AppDbContext context, [FromRoute] int id)
+    {
+        var person = await context.Persons.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+        return person == null ? NotFound() : Ok(person);
     }
 }
