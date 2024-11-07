be6b785ca33911bc8ce99cf13e5b47861d6b2941
BrunoFreschiAgger <brunofreschi@agger.com.br>
2024-11-07 10:48:05 -0300

    create

---
+++ SimpleAPI/ViewModels/CreatePersonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleAPI.ViewModels;
+
+public class CreatePersonViewModel
+{
+    [Required]
+    public required string Name { get; set; }
+    [Required]
+    public required string Phone { get; set; }
+    [Required]
+    public required string Adress { get; set; }
+}
